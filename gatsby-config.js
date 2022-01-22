b73a7d9921af5cf6dc831e4f422b92776e4f3e67
Roberto Perez <rob.perez.flores@gmail.com>
2022-01-21 20:17:14 -0800

    Imported About Component and some images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Roberto Perez Portfolio`,
+    description: `Portfolio Website for Roberto Perez, Web Developer, Tech Enthusiast, Closet Nerd, Husband and Dad`,
+    author: `Roberto Perez`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    keywords: `Web Design, Portfolio, Web Developer, San Diego, Brevity Is, Software Developer, Font-End Web Developer`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +30,31 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-121190454-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+
 import Layout from "../components/layout"
+import About from "../components/About"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <About></About>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
