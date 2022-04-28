7c3619b49dd972418917c13811751fd065e10707
Roberto Perez <rob.perez.flores@gmail.com>
2022-04-28 12:06:25 -0700

    Added Google Ownership Tag

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="google-site-verification" content="5cID6b2O7xs5SWDMhLQpqn4yForTfCMnElczsugNEgw" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
